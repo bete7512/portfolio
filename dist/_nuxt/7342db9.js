4fa360fbb027eabcdecb8ee595e7db3c08122c55
bete7512 <betekbebe@gmail.com>
2022-10-17 17:33:12 +0300

    modal

---
+++ dist/_nuxt/7342db9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{361:function(n,t,e){"use strict";e.r(t);var c=e(0),r=Object(c.b)({__name:"Projects",setup:function(n){return{__sfc:!0}}}),o=e(9),component=Object(o.a)(r,(function(){var n=this._self._c;this._self._setupProxy;return n("div")}),[],!1,null,null,null);t.default=component.exports}}]);
