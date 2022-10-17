b741a6c894424c6720161c1b7721dc8bdec0d342
bete7512 <betekbebe@gmail.com>
2022-10-17 09:23:20 +0300

    portfolio

--- components/shared/header/Home/Projects.vue
+++ components/shared/header/Home/Projects.vue
@@ -1,77 +1,126 @@
 <template>
     <div>
-        <div class="text-2xl sm:w-48 flex justify-center  mx-2 sm:mx-auto font-bold p-5 bg-gray-500 rounded p-3-lg px-8">
+        <div class="text-2xl sm:w-64 flex justify-c32ter  mx-2 sm:mx-auto font-bold p-5 bg-gray-500 rounded p-3-lg px-8">
             Projects
         </div>
-        <div class="mt-3">
+        <div class="mt-3 container">
             <div class="container  flex flex-wrap gap-6">
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>   <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>   <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>   <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <div class="mx-auto">
+
+                     <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+                 </div>  
+
                 </div>
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <di>
+                 </di>  
+                  <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+
                 </div>
+    
                 <div class="rounded p-3 shadow-lg">
-                    <img class="w-48 rounded p-3 h-20" src="~/assets/portfolio/solidity.png" alt="yelem">
+                    <img class="w-64 rounded p-3 h-32" src="~/assets/portfolio/solidity.png" alt="yelem">
                     <div>
                         <div>Food Recipe website</div>
                         <div>here is the description</div>
                     </div>
+                 <di>
+                 </di class="mx-auto"v>   <button class="mx-auto rounded-lg px-7 py-3 bg-orange-400 ">Visist Project</button>
+
                 </div>
+    
             </div>
         </div>
     </div>
